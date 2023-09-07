6c3c4836b51568a3899fb8c75c7ce4a8ac0fac7a
Oleh Budik <olegbudik@ukr.net>
2023-09-07 18:25:01 +0300

    Исправлено открытие FAQ

--- src/components/main/MainFaQ.js
+++ src/components/main/MainFaQ.js
@@ -29,19 +29,35 @@ export default function MainFaQ() {
                         <p>Don’t see an answer to your question below? Contact our friendly support team for assistance</p>
                     </div>
                     <div className='faq__block'>
-                        <div className='faq__block__block'>
+                        <div className='faq__block__left'>
                             {dataFAQ.map((item, index) => {
-                                return  <div className='faq__item' onClick={() => closeFAQItem(index)} key={index}>
-                                            <details >
-                                                <summary>
-                                                    <h3>{item.question} 
-                                                    </h3>
-                                                </summary>
-                                                <div className='faq__item__title'>
-                                                    <p>{item.answer}</p>
-                                                </div>
-                                            </details>
-                                        </div>
+                                if (index % 2)  return  <div className='faq__item' onClick={() => closeFAQItem(index)} key={index}>
+                                                            <details >
+                                                                <summary>
+                                                                    <h3>{item.question} 
+                                                                    </h3>
+                                                                </summary>
+                                                                <div className='faq__item__title'>
+                                                                    <p>{item.answer}</p>
+                                                                </div>
+                                                            </details>
+                                                        </div>
+                                } 
+                            )}
+                        </div>
+                        <div className='faq__block__right'>
+                            {dataFAQ.map((item, index) => {
+                                if (index % 2 !== 0)  return  <div className='faq__item' onClick={() => closeFAQItem(index)} key={index}>
+                                                            <details >
+                                                                <summary>
+                                                                    <h3>{item.question} 
+                                                                    </h3>
+                                                                </summary>
+                                                                <div className='faq__item__title'>
+                                                                    <p>{item.answer}</p>
+                                                                </div>
+                                                            </details>
+                                                        </div>
                                 } 
                             )}
                         </div>
